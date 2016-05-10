51e0f599f96823265886048d679bd9ac63f215a1
Michael Callan <mcallan@uci.edu>
2016-05-10 14:20:17 -0700

    First push

---
+++ Lab3_12912150_16004325_29856616.c
@@ -0,0 +1,37 @@
+// Michael R. Callan III 12912150
+// James Albert III 16004325
+// John Reissmueller 29856616
+
+struct BlockList {
+  int size;
+  bool allocated;
+  void* start;
+  void* end;
+}
+
+int main () {
+
+  return 0;
+}
+
+
+int allocate () {
+
+  return 0;
+}
+
+void free () {
+
+  return;
+}
+
+BlockList blockList () {
+  BlockList toReturn;
+
+
+  return toReturn;
+}
+
+void writeHeap () {
+
+}
