dccf6f9d1f79d504ba573056f0b59e75ecd50617
jamesalbert <jamesrobertalbert@gmail.com>
2016-05-10 19:09:15 -0700

    setup and signatures

--- Lab3_12912150_16004325_29856616.c
+++ Lab3_12912150_16004325_29856616.c
@@ -2,6 +2,23 @@
 // James Albert III 16004325
 // John Reissmueller 29856616
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAXBLOCKS 128 // maximum amount of blocks
+#define MAXARGS 128   // maximum amount of arguments
+#define MAXCMD 255    // maximum length of command
+
+int parsecommand(char*, char* opts[MAXARGS]);
+int Allocate(int);
+void Free(int);
+struct BlockList* blockList();
+void writeHeap(int, char, int);
+char* printHeap(int, int);
+void quit();
+
 struct BlockList {
   int size;
   char allocated;
