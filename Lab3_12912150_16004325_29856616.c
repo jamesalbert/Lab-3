7080d81e9bee88ea1ab8d888ba0fc35ab5e2c13c
jamesalbert <jamesrobertalbert@gmail.com>
2016-05-10 19:10:01 -0700

    added parsecommand

--- Lab3_12912150_16004325_29856616.c
+++ Lab3_12912150_16004325_29856616.c
@@ -31,6 +31,25 @@ int main () {
   return 0;
 }
 
+int parsecommand(char* command, char* opts[MAXARGS]) {
+  /* parsecommand
+      @command: full command from prompt
+      @opts: options to binary
+
+      takes a full command (eg service nginx start) and places the
+      command name (service) and arguments ([nginx, start]) in opts.
+  */
+  int argc = 0;
+  char* delim = " \t\n";
+  char* tok = strtok(command, delim);
+  // first argument of opts must be the filename with path
+  opts[argc] = malloc(strlen(command)*sizeof(char));
+  strcpy(opts[argc], tok); // append command to binary name
+  for (tok = strtok(NULL, delim); tok != NULL; tok = strtok(NULL, delim))
+    opts[++argc] = tok;
+  opts[argc+1] = NULL; // last element must be null for execve
+  return argc;
+}
 
 int allocate () {
 
